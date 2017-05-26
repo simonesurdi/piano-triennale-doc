0a4b6de980b5ad7c5a505deb59f8e12fe9d37743
Davide Porrovecchio <davide.porrovecchio@agid.gov.it>
2017-05-26 10:32:00 +0200

    Theme update

--- conf.py
+++ conf.py
@@ -81,8 +81,8 @@ html_theme_path = ["_themes", ]
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 if not on_rtd:  # only import and set the theme if we're building docs locally
-    html_theme = 'sphinx_italia_theme'
-    html_theme_path = ["_themes", ]
+    html_theme = 'sphinx_italia_theme',
+    html_theme_path = ["_themes", ],
     html_context = {
         'reference_project': 'Trasformazione digitale',
         'reference_project_url': 'https://trasformazionedigitale.italia.it/',
